476fbcb03dc74ac90e199bada7279e5f679b541d
n451 <neo451>
2024-02-06 00:17:26 +0800

    update api for plugin

--- vimplugin.lua
+++ vimplugin.lua
@@ -2,7 +2,7 @@
 
 package.path = package.path .. ";./lib/?.lua"
 package.cpath = package.cpath .. ";./lib/?.so"
-local p = require "posix"
+local p = require("posix")
 
 local function send(code)
 	fd = p.socket(p.AF_INET, p.SOCK_DGRAM, 0)
@@ -10,13 +10,11 @@ local function send(code)
 	p.close(fd)
 end
 
-
 function worp(what)
+	local b = vim.api.nvim_buf_get_lines(0, 0, -1, false)
 
-	local b = vim.buffer()
-
-	local from = vim.firstline
-	local to = vim.lastline
+	local from = b[1]
+	local to = b[#b]
 
 	if what == "stop" then
 		from, to = 0, 0
@@ -25,10 +23,10 @@ function worp(what)
 		from = 1
 		to = #b
 	elseif what == "paragraph" then
-		while from > 1 and b[from-1]:find("%S") do
+		while from > 1 and b[from - 1]:find("%S") do
 			from = from - 1
 		end
-		while to < #b and b[to+1]:find("%S") do
+		while to < #b and b[to + 1]:find("%S") do
 			to = to + 1
 		end
 	elseif what == "function" then
@@ -40,28 +38,27 @@ function worp(what)
 		end
 	end
 
-	vim.command("sign define sent text=┆ texthl=nonText")
-	vim.command("sign unplace *")
+	vim.cmd("sign define sent text=┆ texthl=nonText")
+	vim.cmd("sign unplace *")
 
 	if from > 0 then
 		local code = {}
 		for i = from, to do
-			vim.command("sign place " .. i .. " line=" .. i .. " name=sent file=" .. b.fname)
-			code[#code+1] = b[i]
+			vim.cmd("sign place " .. i .. " line=" .. i .. " name=sent file=" .. b.fname)
+			code[#code + 1] = b[i]
 		end
-		code[#code+1] = "-- live " .. from .. " " .. to .. " " .. (b.name or "")
+		code[#code + 1] = "-- live " .. from .. " " .. to .. " " .. (b.name or "")
 		send(table.concat(code, "\n"))
 	else
-		vim.command("sign place 1 line=1 name=sent file=" .. b.fname)
+		vim.cmd("sign place 1 line=1 name=sent file=" .. b.fname)
 	end
-
 end
 
-vim.command(':noremap ,a :lua worp("all")<CR>')
-vim.command(':noremap ,f :lua worp("function")<CR>')
-vim.command(':noremap ,p :lua worp("paragraph")<CR>')
-vim.command(':noremap ,v :lua worp("visual")<CR>')
-vim.command(':noremap ,, :lua worp("stop")<CR>')
-vim.command(':noremap <CR> :lua worp("line")<CR>')
+vim.cmd(':noremap ,a :lua worp("all")<CR>')
+vim.cmd(':noremap ,f :lua worp("function")<CR>')
+vim.cmd(':noremap ,p :lua worp("paragraph")<CR>')
+vim.cmd(':noremap ,v :lua worp("visual")<CR>')
+vim.cmd(':noremap ,, :lua worp("stop")<CR>')
+vim.cmd(':noremap <CR> :lua worp("line")<CR>')
 
 -- vi: ft=lua ts=3 sw=3
